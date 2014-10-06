ee31b1131c8bc34f61c1eb2df940edabbf882545
gergo-debreczeni <gergo.debreczeni@yahoo.com>
2014-10-06 17:24:08 +0300

    Added ps helpers for bridge TC

---
+++ setupscripts/bridge_setup.ps1
@@ -0,0 +1,18 @@
+﻿param([string] $vm1, [string] $vm2, [string] $vm3)
+
+function GetVMSwitch([string] $type)
+{
+    $vmswitch = Get-VMSwitch | Where-Object -FilterScript {$_.SwitchType -Eq $type}
+    return $vmswitch
+}
+
+$vmswitch = GetVMSwitch("Private")
+
+$pr1 = $vmswitch[0].Name
+$pr2 = $vmswitch[1].Name
+
+Add-VMNetworkAdapter -VMName $vm1 -SwitchName $pr1
+Add-VMNetworkAdapter -VMName $vm2 -SwitchName $pr1
+Add-VMNetworkAdapter -VMName $vm2 -SwitchName $pr2
+Set-VMNetworkAdapter $vm2 -MacAddressSpoofing on
+Add-VMNetworkAdapter -VMName $vm3 -SwitchName $pr2
---
+++ setupscripts/create-private-vSwitch.ps1
@@ -0,0 +1,11 @@
+﻿function CreateVMSwitch([string] $type)
+{
+    $privateSwitches = Get-VMSwitch | Where-Object -FilterScript {$_.SwitchType -Eq $type}
+    if( $privateSwitches.Length -eq 0)
+    {
+        New-VMSwitch -Name "private1" -SwitchType $type
+        New-VMSwitch -Name "private2" -SwitchType $type
+    }
+}
+
+CreateVMSwitch("Private")
