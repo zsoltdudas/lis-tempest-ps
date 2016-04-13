f101d1d48a16812bf72541333fd1193453dbe60e
Zsolt Dudás <zdudas@cloudbasesolutions.com>
2016-04-13 14:33:41 +0300

    Detach disk check improvement

--- setupscripts/detach-disk.ps1
+++ setupscripts/detach-disk.ps1
@@ -31,7 +31,7 @@ if (!$vhdxDisks)
 foreach ($vhdx in $vhdxDisks)
 {
 	$vhdxPath = $vhdx.Path
-	if ($vhdxPath -match $vhdxName)
+	if ($vhdxPath.Contains($vhdxName) -or $vhdxPath.Contains('Target'))
 	{
 		"Info : Removing drive ${vhdxName}"
         $vhdx.ControllerLocation
